eb40c46f4222a2a6873cbe8de9e4094f0aa04ce8
Shyam Ghallay <02230225.cst@rub.edu.bt>
2024-02-29 16:30:08 +0530

    change to class_activity 1.py

--- class_activity 1.py
+++ class_activity 1.py
@@ -1,4 +1,4 @@
-print("Hello world!!")
+#classwork 1
 
 name = input("Enter the name:")
 num1 = float(input("Enter the number:"))
@@ -8,3 +8,7 @@ additional_result = num1 + num2
 print("additional_result:\n",additional_result)
 subtraction_result = num1 - num2
 print("subtraction_result:\n",subtraction_result)
+division_result = num2/num1
+print("division_result:\n", division_result)
+Multiplication_result = num1*num2
+print("Multiplication_result:\n", Multiplication_result)
