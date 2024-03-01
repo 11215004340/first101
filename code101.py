62a289a75d2e240ca1b20b53ad912d569a30b5d9
Shyam Ghallay <02230225.cst@rub.edu.bt>
2024-03-01 11:47:50 +0530

    chamge to code101.py

--- code101.py
+++ code101.py
@@ -1,10 +1,3 @@
-print("Hello world!!")
-
-name = input("Enter the name:")
-num1 = float(input("Enter the number:"))
-num2 = float(input("Enter the number:"))
-print("hello", name)
-additional_result = num1 + num2
-print("additional_result:\n",additional_result)
-subtraction_result = num1 - num2
-print("subtraction_result:\n",subtraction_result)
+print("Hello world!")
+print("I m happy i exist here.")
+print("Thank you")
---
+++ first task.py
@@ -0,0 +1,9 @@
+#Exercise 1
+
+age =int(input("Enter your age:"))
+student = input("Are you student? (yes/no):")
+
+if age<=12 and student =="yes":
+    print("You are eligible for discount on the movie ticket.")
+elif age>13 and age<=18:
+    print("You are eligible for discount on the movie ticket.")
