b61eb18b837d5caa6052797dbd9bd2f68a8dd519
Shyam Ghallay <02230225.cst@rub.edu.bt>
2024-03-18 00:15:52 +0530

    change to first task.py

--- first task.py
+++ first task.py
@@ -1,9 +1,2 @@
 #Exercise 1
 
-age =int(input("Enter your age:"))
-student = input("Are you student? (yes/no):")
-
-if age<=12 and student =="yes":
-    print("You are eligible for discount on the movie ticket.")
-elif age>13 and age<=18:
-    print("You are eligible for discount on the movie ticket.")
