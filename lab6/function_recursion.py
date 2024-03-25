53523c53e8f70f8645ee6565eeac4e4b04e9c9ea
Shyam Ghallay <02230225.cst@rub.edu.bt>
2024-03-25 12:55:28 +0530

    change to function_recursion.py

---
+++ lab6/function_recursion.py
@@ -0,0 +1,11 @@
+def sum_of_digits(n):
+    if n < 10:
+        return n
+    
+    else:
+        last_digits = n % 10
+        remaining_digits = n // 10
+
+        return last_digits + sum_of_digits(remaining_digits)
+    
+print(sum_of_digits(1234))
