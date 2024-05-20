987488c06e29c526a4e84e3f0ec2458ec5e9ef2a
Shyam Ghallay <02230225.cst@rub.edu.bt>
2024-05-20 15:49:49 +0530

    lab 4 exercise

--- classwork 2.py
+++ classwork 2.py
@@ -6,12 +6,15 @@ student_name=input("Enter student name: ")
 student_age=int(input("Enter student age: "))
 student_grade=int(input("Enter student grade: "))
 student_list.append(student_name)
+
 #print(student_list)
 student_info["Age:"]=student_age
 student_info["Grade:"]=student_grade
 student_dict.update({student_name:student_info})
+
 #print(student_info)
 print("Student information added successfully")
+
 #searching students
 search_name=input("Enter the name to search or simply click enter to skip: ")
 if search_name in student_list:
