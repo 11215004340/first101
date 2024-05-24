c8cfad3c69d7b2869883d6dec89da292feb9151b
Shyam Ghallay <02230225.cst@rub.edu.bt>
2024-05-24 23:37:05 +0530

    lab 11 climbing stair classwork

--- climbing.stair1.py
+++ climbing.stair1.py
@@ -1,17 +1,14 @@
 class Solution(object):
-    def climbStairs(self, n):
+    def climeStairs (self, n): 
         memo = {}
-
         return self.ways(n, memo)
-    
-    def ways(self, n, memo):
-
-        if n is memo:
-            return memo[n]       
+    def ways(self, n , memo):
+        if n in memo:
+            return memo[n]
         if n == 1:
             return 1
         elif n == 2:
             return 2
-        
-        memo[n] = self.ways(n -1, memo) + self.ways(n - 2, memo)
+        memo[n] = self.ways(n - 1, memo) + self.ways(n - 2, memo)
         return memo[n]
+    
