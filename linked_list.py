0e8555f77f85cc62a4abfaa639c815f245987943
Shyam Ghallay <02230225.cst@rub.edu.bt>
2024-05-24 23:28:18 +0530

    lab 10 linked list

--- linked_list.py
+++ linked_list.py
@@ -1,32 +1,72 @@
-class Node: # method for the Node class. Node will have two attributes: data and next. 
-    def __init__(self, data): # Initialize the data attribute of the node with the given data. 
-        self.data = data # Initialize the next attribute of the node as None, indicating that it initially does not point to any other node as its empty. 
-        self.next = None # Define a class named SinglyLinkedList representing a singly linked list. 
-
-class SinglyLinkedList: # Define the constructor method for the SinglyLinkedList class. 
-    def __init__(self): # Initialize the head attribute of the linked list as None, indicating an empty list. 
-        self.head = None # Define a method named append to add a new node with the given data to the end of the linked list. 
-    
-    def append(self, data): # Create a new node object with the given data. 
-        new_node = Node(data) # Check if the linked list is empty.
-        if not self.head: # If the linked list is empty, set the new node as the head of the linked list and return. 
-            self.head = new_node 
-            return # If the linked list is not empty, traverse the linked list to find the last node. 
-        last_node = self.head 
-        while last_node.next: 
-            last_node = last_node.next # Once the last node is found, set the next attribute of the last node to the new node, effectively adding it to the end of the linked list. 
-        last_node.next = new_node # Define a method named display to print the elements of the linked list. 
-    
-    def display(self): # Start from the head of the linked list. 
-        current = self.head # Traverse the linked list and print the data of each node. 
-        while current: 
-            print(current.data, end="-> ") # Move to the next node in the linked list. 
-            current = current.next # Print "None" to signify the end of the linked list. 
-        print("None")  
-
-sll = SinglyLinkedList() # Append some elements to the linked list. 
-sll.append(1) 
-sll.append(2) 
-sll.append(3) # Display the elements of the linked list. 
+#single linked list
+class Nodes:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class singlyLinkedList:
+    def __init__(self):
+        self.head=None
+    def append(self,data):
+        new_node= Nodes(data)
+        if not self.head:
+            self.head=new_node
+            return
+        last_node= self.head
+        while last_node.next:
+            last_node= last_node.next
+        last_node.next= new_node
+    def display(self):
+        current= self.head
+        while current:
+            print(current.data, end=" ->")
+            current= current.next
+        print("None")
+sll= singlyLinkedList()
+sll.append(1)
+sll.append(2)
+sll.append(3)
 sll.display()
-jdjdjdjcncncjcn
+
+#Reversed linked list
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        curr = head
+        while curr:
+            next_node= curr.next
+            curr.next = prev
+            prev= curr
+            curr = next_node
+        return prev
+    
+#Merge two sorted lists
+class Solution(object):
+    def mergeTwoLists(self,list1,list2, ListNode):
+        dummy = ListNode(-1)
+        current = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current= current.next
+        if list1:
+            current.next = list1
+        elif list2:
+            current.next = list2
+        return dummy.next
+    
+#Remove Nth Node from End to List
+class Solution(object):
+    def removeNthFromEnd(self, head, n, ListNode):
+        dummy = ListNode (0, head)
+        slow = fast = dummy
+        for i in range(n):
+            fast=fast.next
+        while fast.next:
+            slow= slow.next
+            fast = fast.next
+            slow.next = slow.next.next
+            return dummy.next
